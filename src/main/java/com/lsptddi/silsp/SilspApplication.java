24a3336880f7fcae5b6b6e3ebade73f7bc5a5af6
Muhammad Satria Arrozak <satria30803@gmail.com>
2025-08-30 17:30:54 +0700

    First Upload Project

---
+++ src/main/java/com/lsptddi/silsp/SilspApplication.java
@@ -0,0 +1,13 @@
+package com.lsptddi.silsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lsptddi/silsp/SilspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsptddi.silsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

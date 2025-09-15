c9dc61cbdaff052a8522e6a8fd27aa965382ed60
Muhammad Satria Arrozak <satria30803@gmail.com>
2025-09-15 11:48:30 +0700

    tab add skema

--- src/main/resources/static/dist/js/adminlte.js
+++ src/main/resources/static/dist/js/adminlte.js
@@ -4263,6 +4263,7 @@ class AccessibilityManager {
   }
 }
 
+// expand and collapse table rows
 document.addEventListener("DOMContentLoaded", function () {
   // Ambil semua tombol yang berfungsi sebagai pemicu expand/collapse
   const expandButtons = document.querySelectorAll(".btn-expand");
@@ -4297,3 +4298,105 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+// PDF Preview in Modal
+$(document).ready(function () {
+  // Event ini akan dijalankan SETIAP KALI modal #previewModal akan ditampilkan
+  $("#previewModal").on("show.bs.modal", function (event) {
+    // Dapatkan tombol yang memicu modal
+    var button = $(event.relatedTarget);
+
+    // Ekstrak path file dari atribut data-filepath
+    var filePath = button.data("filepath");
+
+    // Dapatkan elemen modal itu sendiri
+    var modal = $(this);
+
+    // Cari elemen iframe di dalam modal dan atur atribut 'src'-nya
+    modal.find("#pdf-viewer").attr("src", filePath);
+  });
+
+  // (Opsional) Kosongkan src iframe saat modal ditutup agar tidak membebani browser
+  $("#previewModal").on("hidden.bs.modal", function () {
+    $(this).find("#pdf-viewer").attr("src", "");
+  });
+});
+
+
+// Date and Select 
+$(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+
+    //Initialize Select2 Elements
+    $('.select2bs4').select2({
+      theme: 'bootstrap4'
+    })
+
+    //Datemask dd/mm/yyyy
+    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    //Datemask2 mm/dd/yyyy
+    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    //Money Euro
+    $('[data-mask]').inputmask()
+
+    //Date picker
+    $('#reservationdate').datetimepicker({
+        format: 'L'
+    });
+
+    //Date and time picker
+    $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
+
+    //Date range picker
+    $('#reservation').daterangepicker()
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({
+      timePicker: true,
+      timePickerIncrement: 30,
+      locale: {
+        format: 'MM/DD/YYYY hh:mm A'
+      }
+    })
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+      {
+        ranges   : {
+          'Today'       : [moment(), moment()],
+          'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+          'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        startDate: moment().subtract(29, 'days'),
+        endDate  : moment()
+      },
+      function (start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+      }
+    )
+
+    //Timepicker
+    $('#timepicker').datetimepicker({
+      format: 'LT'
+    })
+
+    //Bootstrap Duallistbox
+    $('.duallistbox').bootstrapDualListbox()
+
+    //Colorpicker
+    $('.my-colorpicker1').colorpicker()
+    //color picker with addon
+    $('.my-colorpicker2').colorpicker()
+
+    $('.my-colorpicker2').on('colorpickerChange', function(event) {
+      $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
+    })
+
+    $("input[data-bootstrap-switch]").each(function(){
+      $(this).bootstrapSwitch('state', $(this).prop('checked'));
+    })
+
+  })

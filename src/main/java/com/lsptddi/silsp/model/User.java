2e312e54eef8790eec45ebfcf9988188579e1e36
Muhammad Satria Arrozak <satria30803@gmail.com>
2025-11-26 11:36:50 +0700

    user admin list9

--- src/main/java/com/lsptddi/silsp/model/User.java
+++ src/main/java/com/lsptddi/silsp/model/User.java
@@ -25,8 +25,9 @@ public class User {
     private String password;
 
     // Relasi ke Role (Anggap Anda sudah punya entity Role)
-    // @ManyToMany(fetch = FetchType.EAGER)
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL) // <--- PASTIKAN ADA cascade = CascadeType.ALL
+    @ManyToMany(fetch = FetchType.EAGER)
+    // @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL) // <---
+    // PASTIKAN ADA cascade = CascadeType.ALL
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
